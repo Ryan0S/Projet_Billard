e3e6e900abd3bd1a7b65b085b5dd60239c84d52b
Ryan0S <94574050+Ryan0S@users.noreply.github.com>
2023-03-07 19:47:38 +0100

    Update RemoveOutlier.m

--- RemoveOutlier.m
+++ RemoveOutlier.m
@@ -1,11 +1,19 @@
 function [X,Y]=RemoveOutlier(X,Y)
 
 sugma = isoutlier(X,"movmedian",10);
-IdOutlierX = find(sugma == 1);
+IdOutlierX = find(sugma);
+if IdOutlierX(1) == 1
+    IdOutlierX(1) = [];
+end
 PrevIdOutlierX = IdOutlierX - 1;
-X(IdOutlierX)=X(PrevIdOutlierX);
+X(IdOutlierX)= X(PrevIdOutlierX);
 
 ligma = isoutlier(Y,"movmedian",10);
-IdOutlierY = find(ligma == 1);
+IdOutlierY = find(ligma)
+if IdOutlierY(1) == 1
+    IdOutlierY(1) = [];
+end
 PrevIdOutlierY = IdOutlierY - 1;
 Y(IdOutlierY)=Y(PrevIdOutlierY);
+
+end
