67242f75911f93a09b63b5afdf7b1100c5e8ccdd
RomainEPFL <126600910+RomainEPFL@users.noreply.github.com>
2023-03-14 18:13:56 +0100

    Update GetFirstMoveIdx.m

--- GetFirstMoveIdx.m
+++ GetFirstMoveIdx.m
@@ -7,12 +7,12 @@ S2 = Y(1)-Y;%différence entre la première valeur de Y et toutes les autres
 
 S = sqrt(S1.^2 + S2.^2);%faire la norme de ces valeurs
 
-cbt = find(S >= MoveDistPx);%matrice remplie des indices des frames auxquelles les boules bougent
+cbt = find(S > MoveDistPx);%matrice remplie des indices des frames auxquelles les boules bougent
 
 FirstMoveIdx = cbt(1)%premier élément de cette matrice
 
 %PARTIE 2
 
-Dist = sqrt((X(FirstMoveIdx+1)-X(FirstMoveIdx))^2+(Y(FirstMoveIdx+1)-Y(FirstMoveIdx)^2);
+Dist = sqrt((X(FirstMoveIdx)-X(FirstMoveIdx-1))^2+(Y(FirstMoveIdx)-Y(FirstMoveIdx-1)^2);
 
 end
