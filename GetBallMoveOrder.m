d331774707388e93226bfe667b554d90946bc718
Ryan0S <94574050+Ryan0S@users.noreply.github.com>
2023-05-21 23:04:43 +0200

    Update GetBallMoveOrder.m

--- GetBallMoveOrder.m
+++ GetBallMoveOrder.m
@@ -1,16 +1,16 @@
 function [FirstBall,SecondBall,LastBall, NbBallsMoved] = GetBallMoveOrder(Xr, Yr, Xy, Yy, Xw, Yw, MoveDistPx)
-
+  
 [IdR, DistR] = GetFirstMoveIdx(Xr,Yr, MoveDistPx);
 [IdY, DistY] = GetFirstMoveIdx(Xy,Yy, MoveDistPx);
 [IdW, DistW] = GetFirstMoveIdx(Xw,Yw, MoveDistPx);
 
 Id = [IdR IdY IdW];
 
-cbt = [IdR -DistR 1; IdY -DistY 2; IdW -DistW 3];
-cock = sortrows(cbt);
-FirstBall = cock(1,3);
-SecondBall = cock(2,3);
-LastBall = cock(3,3);
+TempMatrix = [IdR -DistR 1; IdY -DistY 2; IdW -DistW 3];
+TempMatrix = sortrows(TempMatrix);
+FirstBall = TempMatrix(1,3);
+SecondBall = TempMatrix(2,3);
+LastBall = TempMatrix(3,3);
 NbBallsMoved = length(Id)
 
 end
